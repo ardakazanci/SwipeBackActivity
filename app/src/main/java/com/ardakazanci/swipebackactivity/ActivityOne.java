c1c94afd3c574c45a2c304bdca44299424b9ab80
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-08-09 20:07:48 +0300

    Initial commit

---
+++ app/src/main/java/com/ardakazanci/swipebackactivity/ActivityOne.java
@@ -0,0 +1,17 @@
+package com.ardakazanci.swipebackactivity;
+
+
+import android.os.Bundle;
+
+import com.liuguangqiang.swipeback.SwipeBackActivity;
+import com.liuguangqiang.swipeback.SwipeBackLayout;
+
+public class ActivityOne extends SwipeBackActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_one);
+		setDragEdge(SwipeBackLayout.DragEdge.LEFT);
+	}
+}
